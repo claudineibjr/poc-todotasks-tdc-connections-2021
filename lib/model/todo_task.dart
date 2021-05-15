9d3ac35dc67913cd132ac1a5ba2503b79323b5ac
Claudinei B Jr <claudinei.brito@monitoratec.com.br>
2021-05-15 11:30:13 -0300

    Implementing serialization at TodoTask model

--- lib/model/todo_task.dart
+++ lib/model/todo_task.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:poc_todotasks_team/model/User.dart';
+import 'package:poc_todotasks_team/model/serializable.dart';
 
-class TodoTask {
+class TodoTask implements Serializable<TodoTask> {
   final int id;
   final User responsable;
   final DateTime creationDate;
@@ -17,4 +18,79 @@ class TodoTask {
     this.finishDate,
     @required this.tasksToDo,
   });
+
+  // #region Serialization
+  // #region FromJson
+  factory TodoTask.fromJsonFactory(Map<String, dynamic> json) {
+    User responsable;
+    if (json['responsable'] != null) {
+      responsable = User.fromJsonFactory(
+        json['responsable'],
+      );
+    }
+
+    DateTime expectedFinishDate;
+    if (json['expectedFinishDate'] != null) {
+      expectedFinishDate = DateTime.parse(
+        json['expectedFinishDate'],
+      );
+    }
+
+    DateTime finishDate;
+    if (json['finishDate'] != null) {
+      finishDate = DateTime.parse(
+        json['finishDate'],
+      );
+    }
+
+    return TodoTask(
+      id: json['id'],
+      responsable: responsable,
+      creationDate: DateTime.parse(
+        json['creationDate'],
+      ),
+      expectedFinishDate: expectedFinishDate,
+      finishDate: finishDate,
+      tasksToDo: List<String>.from(
+        json['tasksToDo'].map(
+          (taskToDo) => taskToDo,
+        ),
+      ),
+    );
+  }
+
+  factory TodoTask.fromRawJson(String rawJson) =>
+      Serializable.serializableFromRawJson(
+        rawJson,
+        fromJson,
+      );
+
+  static TodoTask fromJson(Map<String, dynamic> json) =>
+      TodoTask.fromJsonFactory(
+        json,
+      );
+  // #endregion
+  // #region ToJson
+  @override
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'responsable': responsable?.toJson() ?? null,
+      'creationDate': creationDate.toIso8601String(),
+      'expectedFinishDate': expectedFinishDate?.toIso8601String() ?? null,
+      'finishDate': finishDate?.toIso8601String() ?? null,
+      'tasksToDo': List<dynamic>.from(
+        tasksToDo.map(
+          (taskToDo) => taskToDo,
+        ),
+      ),
+    };
+  }
+
+  @override
+  String toRawJson() => Serializable.serializableToRawJson(
+        toJson(),
+      );
+  // #endregion
+  // #endregion
 }
