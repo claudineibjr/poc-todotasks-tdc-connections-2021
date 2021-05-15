7dba56a508ff82f7b46fc9dbb93cbaf1c482d10f
Claudinei B Jr <claudinei.brito@monitoratec.com.br>
2021-05-15 11:36:53 -0300

    Creation TodoTaskInput class

---
+++ lib/model/input/todo_task_input.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:poc_todotasks_team/model/todo_task.dart';
+
+class TodoTaskInput {
+  final DateTime expectedFinishDate;
+  final List<String> tasksToDo;
+
+  TodoTaskInput({
+    this.expectedFinishDate,
+    @required this.tasksToDo,
+  });
+
+  factory TodoTaskInput.fromTodoTaskFactory(TodoTask todoTask) {
+    return TodoTaskInput(
+      expectedFinishDate: todoTask.expectedFinishDate,
+      tasksToDo: todoTask.tasksToDo,
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'expectedFinishDate': expectedFinishDate?.toIso8601String() ?? null,
+      'tasksToDo': List<dynamic>.from(
+        tasksToDo.map(
+          (taskToDo) => taskToDo,
+        ),
+      ),
+    };
+  }
+}
