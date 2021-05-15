2630c822cf60ccba2b0df868e7ba7182bab70e88
Claudinei B Jr <claudinei.brito@monitoratec.com.br>
2021-05-15 11:20:38 -0300

    Implementing serialization at User model

--- lib/model/user.dart
+++ lib/model/user.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:poc_todotasks_team/model/serializable.dart';
 
-class User {
+class User implements Serializable<User> {
   int id;
   String name;
   String surname;
@@ -12,4 +13,44 @@ class User {
     @required this.surname,
     @required this.email,
   });
+
+  // #region Serialization
+  // #region FromJson
+  factory User.fromJsonFactory(Map<String, dynamic> json) {
+    return User(
+      id: json['id'],
+      name: json['name'],
+      surname: json['surname'],
+      email: json['email'],
+    );
+  }
+
+  factory User.fromRawJson(String rawJson) =>
+      Serializable.serializableFromRawJson(
+        rawJson,
+        fromJson,
+      );
+
+  static User fromJson(Map<String, dynamic> json) => User.fromJsonFactory(
+        json,
+      );
+  // #endregion
+
+  // #region ToJson
+  @override
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'name': name,
+      'surname': surname,
+      'email': email,
+    };
+  }
+
+  @override
+  String toRawJson() => Serializable.serializableToRawJson(
+        toJson(),
+      );
+  // #endregion
+  // #endregion
 }
